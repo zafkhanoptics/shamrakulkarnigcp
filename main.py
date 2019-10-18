2c9e60117a81d47137532fc6aefce12c3e6d4d66
SharmaK <sharma@kulkarni.com>
2019-10-18 11:57:24 +0530

    Second SharmaKulkarni commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 class arun(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello All. Cloud Computing weclomes you')
+        self.response.write('Sharma And Kulkarni welcome you abode the DBDA Express !!!!')
 
 
 app = webapp2.WSGIApplication([
